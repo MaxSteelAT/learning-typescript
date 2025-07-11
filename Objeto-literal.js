a8625bb014c389fcb98bcbb53c20f1f638e0c7aa
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-07-11 14:55:46 -0700

    Tipo de dato: Objeto Lineal

---
+++ Objeto-literal.js
@@ -0,0 +1,12 @@
+"use strict";
+let programador = {
+    nombre: 'Sergio Code',
+    tecnologias: ['React', 'Angular', 'Svelte'],
+    tomaMate: true
+};
+programador = {
+    nombre: 'Ricardo Leal',
+    tecnologias: ['c#'],
+    tomaMate: false
+};
+console.log(programador);
---
+++ Objeto-literal.ts
@@ -0,0 +1,13 @@
+ let programador = {
+  nombre: 'Sergio Code',
+  tecnologias:['React', 'Angular', 'Svelte'],
+  tomaMate: true
+ }
+
+ programador = {
+  nombre: 'Ricardo Leal',
+  tecnologias:['c#'],
+  tomaMate: false
+ }
+
+ console.log(programador)
